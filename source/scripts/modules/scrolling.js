195eb77fb578bb0607c24f1d65369747fda9a1ef
MrAndre18 <andrew09200@mail.ru>
2020-10-01 02:30:16 +0300

    Added other files

---
+++ source/scripts/modules/scrolling.js
@@ -0,0 +1,38 @@
+﻿
+// Скролл наверх 
+
+$('.short-logo').click(function(){
+	if ($(document).scrollTop() > '0') {
+		$('body').animate({scrollTop:0}, 1000);
+		$('html').animate({'scrollTop':0}, 1000);
+	}
+});
+
+// Скроллинг по якорям
+
+const anchors = $('a[data-target^="anchor"]');
+
+anchors.each(function() {
+	$(this).click((event)=>{
+		event.preventDefault();
+
+		const blockID = '' + $(this).attr('href');
+		const top = $(blockID).offset().top;
+		$("body, html").animate({scrollTop: top}, 1000);
+	})
+});
+
+// Кнопка "Заказать сайт"
+
+$('.to-checkout, .checkout-btn').click(() => {
+	$("body, html").animate({scrollTop: $('#contacts').offset().top}, 1000);
+});
+
+$(window).scroll(() => {
+	if ($(window).scrollTop() >= $('#contacts').offset().top - 200) {
+		$('.to-checkout').fadeOut(600, 'linear');
+	}
+	else {
+		$('.to-checkout').fadeIn(1000, 'linear');
+	}
+});
---
+++ source/scripts/modules/slider.js
@@ -0,0 +1,49 @@
+﻿$('.slider').slick({
+	speed: 600,
+	centerMode: true,
+	variableWidth: false,
+	waitForAnimate: false,
+	focusOnSelect: true,
+	swipeToSlide: true,
+	responsive: [
+		{
+			breakpoint: 599,
+			settings: {
+				centerMode: false,
+				waitForAnimate: true,
+				rows: 2,
+				slidesPerRow: 2,
+				swipeToSlide: false,
+			}
+		},
+		{
+			breakpoint: 1023,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 1,
+				waitForAnimate: true,
+				centerMode: false,
+				autoplay: false,
+				autoplaySpeed: 5000,
+				focusOnSelect: false,
+			}
+		}
+	],
+	mobileFirst: true
+});
+$('.slider .slick-prev').html('');
+$('.slider .slick-prev').append($('<img>', {
+	src: 'img/svg/left.svg',
+	alt: 'Влево',
+	class: 'svg prev-example'
+}));
+$('.slider .slick-next').html('');
+$('.slider .slick-next').append($('<img>', {
+	src: 'img/svg/right.svg',
+	alt: 'Вправо',
+	class: 'svg next-example'
+}));
+
+$(document).resize(function() {
+	$('.slider').slick('setPosition');
+});
